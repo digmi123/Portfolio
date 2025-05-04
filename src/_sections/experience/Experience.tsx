22a027ebea93bf0059a299ca2b7a1d7f2ad941da
digmi123 <65775879+digmi123@users.noreply.github.com>
2025-05-04 19:47:44 +0300

    Responsive Experience section.

--- src/_sections/experience/Experience.tsx
+++ src/_sections/experience/Experience.tsx
@@ -2,7 +2,7 @@ import ExperienceItem from "./_components/ExperienceItem";
 
 function Experience() {
   return (
-    <div id="experience-section" className="py-6">
+    <div id="experience-section" className="py-6 h-full">
       <h1 className="text-3xl py-4">Experience</h1>
       <ExperienceItem />
     </div>
--- src/_sections/experience/_components/ExperienceItem.tsx
+++ src/_sections/experience/_components/ExperienceItem.tsx
@@ -1,21 +1,30 @@
+import SkillsTree from "@/_components/SkillsTree";
 import Image from "next/image";
+
+const skills = [
+  { name: "react", path: "./react.svg" },
+  { name: "nodejs", path: "./nodejs.svg" },
+  { name: "gcp", path: "./gcp.svg" },
+  { name: "redux", path: "./redux.svg" },
+  { name: "postgres", path: "./postgres.svg" },
+];
 
 function ExperienceItem() {
   return (
     <div
       id="experience-item"
       // bg-radial-[at_0%_0%] from-white to-zinc-900 to-75%
-      className="flex rounded-md px-6 py-6 shadow-card shadow-[#777777]"
+      className="max-md:flex-wrap h-full flex rounded-md px-6 py-6 shadow-card shadow-[#777777]"
     >
       <div
         id="left-side"
-        className="flex flex-col gap-3 border-r-1 border-[#d7d7d7BF] pr-4"
+        className="max-md:w-full flex flex-col gap-3 pr-4 w-2/6"
       >
         <Image
           src="/gist-logo.webp"
           alt="company-logo"
-          width={250}
-          height={200}
+          width={400}
+          height={400}
         />
         <div id="role-description" className="">
           <h2 className="text-gray-400 font-bold text-xl">
@@ -23,41 +32,52 @@ function ExperienceItem() {
           </h2>
           <p className="text-gray-400 font-medium">10.03.23 - 12.10.24</p>
         </div>
-        <div id="technologies" className="flex items-center gap-4">
-          <Image
-            src="/react.svg"
-            alt="technology"
-            width={30}
-            height={30}
-            className="rounded-full aspect-square object-cover"
-          />
-          <Image
-            src="/nodejs.svg"
-            alt="technology"
-            width={30}
-            height={30}
-            className="rounded-full aspect-square object-cover"
-          />
-          <Image
-            src="/gcp.svg"
-            alt="technology"
-            width={30}
-            height={30}
-            className="rounded-full aspect-square object-cover"
-          />
+        <div
+          id="test-wrapper"
+          className="w-full h-full flex items-center justify-center"
+        >
+          <SkillsTree skills={skills} size={300} spin={true} />
         </div>
       </div>
-      {/* <hr id="divider" className="w-[1px] h-full bg-black" /> */}
-      <div id="right-side" className="flex-1">
-        <p className="px-10 text-xl">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim,
-          quisquam reprehenderit natus accusamus harum necessitatibus quidem
-          ipsam, sapiente, quibusdam quae deserunt omnis minima. Quisquam
-          deleniti consequatur nostrum cum, omnis consequuntur! Lorem ipsum
-          dolor sit amet, consectetur adipisicing elit. Debitis, tenetur. Ad,
-          sunt? Fugiat corrupti at eveniet aspernatur magni, dolore pariatur
-          dicta, voluptate aut corporis hic eius officiis sint ipsa dolor.
-        </p>
+      <div
+        id="right-side"
+        className="max-md:w-full flex-1 flex flex-col gap-4 items-center justify-start"
+      >
+        <ul className="text-xl flex flex-col gap-2 font-medium list-disc pl-5">
+          <li>
+            Worked as a <strong>fullstack developer</strong> on GistMed, a
+            platform for hospitals and patients to manage and track treatment
+            processes.
+          </li>
+          <li>
+            Developed <strong>interfaces for hospital staff</strong> to monitor
+            patients and manage documentation, and{" "}
+            <strong>mobile-friendly screens for patients</strong> to complete
+            dynamic questionnaires.
+          </li>
+          <li>
+            Built a system that generates{" "}
+            <strong>personalized animated videos</strong> based on patient
+            responses by dynamically combining pre-made video segments.
+          </li>
+          <li>
+            Implemented features for{" "}
+            <strong>document printing, signing, and tracking</strong>, enabling
+            seamless interaction between patients and hospital staff.
+          </li>
+        </ul>
+
+        <div
+          id="image-wrapper"
+          className="flex items-center justify-center flex-1 w-full"
+        >
+          <Image
+            src="/trom-hardama.png"
+            alt="experience-teaser"
+            width={600}
+            height={400}
+          />
+        </div>
       </div>
     </div>
   );
