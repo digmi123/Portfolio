b17dd01d2380eddef72e7e2cd6fc31144048cffa
digmi123 <65775879+digmi123@users.noreply.github.com>
2025-05-04 19:45:44 +0300

    Update navbar links to Next Link Tag.

--- src/_components/Navbar.tsx
+++ src/_components/Navbar.tsx
@@ -1,23 +1,23 @@
+"use client";
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <div className="py-6 sticky top-0 z-10">
       <div
         id="nav-list"
-        className="uppercase text-shadow-(--shadow-glow) backdrop-blur bg-[radial-gradient(ellipse_70%_70%_at_50%_50%,#ffffff6f,#6161614f)] flex items-center gap-4 text-background py-3 px-4 rounded-full w-fit m-auto text-xl"
+        // bg-[radial-gradient(ellipse_70%_70%_at_50%_50%,#ffffff6f,#6161614f)]
+        className="bg-white/70 backdrop-blur backdrop-hue-rotate-270 uppercase text-shadow-(--shadow-glow) flex items-center gap-4 text-background py-3 px-6 rounded-full w-fit m-auto text-lg"
       >
-        <a
-          id="nav-link"
-          className="font-medium scale-110"
-          href="#profile-section"
-        >
+        <Link id="nav-link" className="scale-110" href="#profile-section">
           Profile
-        </a>
-        <a id="nav-link" href="#experience-section">
+        </Link>
+        <Link id="nav-link" href="#experience-section">
           Experience
-        </a>
-        <a id="nav-link" href="#projects-section">
+        </Link>
+        <Link id="nav-link" href="#projects-section">
           Projects
-        </a>
+        </Link>
         <p id="nav-link">Contact</p>
       </div>
     </div>
