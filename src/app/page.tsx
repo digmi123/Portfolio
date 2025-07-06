79cc6b303220527026e74d57b67fbf6fdbe0006d
digmi123 <65775879+digmi123@users.noreply.github.com>
2025-07-06 23:47:21 +0300

    Add gradients compoennt between pages.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-import ScreenWrapper from "@/_components/ScreenWrapper";
-import Experience from "@/_sections/experience/Experience";
-import Profile from "@/_sections/profile/Profile";
-import Projects from "@/_sections/projects/Projects";
+import ScreenWrapper from "@/components/ScreenWrapper";
+import Contact from "@/sections/contact/Contact";
+import GradientDivider from "@/sections/experience/components/GradientDivider";
+import Experience from "@/sections/experience/Experience";
+import Profile from "@/sections/profile/Profile";
+import Projects from "@/sections/projects/Projects";
 
 export default function Home() {
   return (
@@ -10,12 +12,22 @@ export default function Home() {
         <Profile />
       </ScreenWrapper>
 
+      <GradientDivider />
+
       <ScreenWrapper>
         <Experience />
       </ScreenWrapper>
 
+      <GradientDivider />
+
       <ScreenWrapper>
         <Projects />
+      </ScreenWrapper>
+
+      <GradientDivider />
+
+      <ScreenWrapper>
+        <Contact />
       </ScreenWrapper>
     </>
   );
