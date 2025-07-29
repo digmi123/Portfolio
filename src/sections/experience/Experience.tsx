fd29b9e20e95010036bd612406a00555e140a2e1
digmi123 <65775879+digmi123@users.noreply.github.com>
2025-07-29 14:03:21 +0300

    Add min-h-screen to experience section.

--- src/sections/experience/Experience.tsx
+++ src/sections/experience/Experience.tsx
@@ -2,7 +2,10 @@ import ExperienceItem from "./components/ExperienceItem";
 
 function Experience() {
   return (
-    <div id="experience-section" className="py-6 flex-1 flex flex-col gap-5">
+    <div
+      id="experience-section"
+      className="py-6 flex-1 flex flex-col gap-5 min-h-screen"
+    >
       <ExperienceItem />
     </div>
   );
