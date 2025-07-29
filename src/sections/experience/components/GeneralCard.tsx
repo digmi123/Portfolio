b6293a44973a2f7ccd4ccec2574fbbe90898424f
digmi123 <65775879+digmi123@users.noreply.github.com>
2025-07-29 14:04:08 +0300

    update active card styling.

--- src/sections/experience/components/GeneralCard.tsx
+++ src/sections/experience/components/GeneralCard.tsx
@@ -13,7 +13,7 @@ export default function GeneralCard({
       id="general-card"
       className={cn(
         "border-animation skills relative bg-black rounded-md overflow-hidden p-4 group before:blur-xl min-h-70",
-        active && "-translate-y-4 shadow-[0_0_6px_#ffffff]"
+        active && "-translate-y-6"
       )}
     >
       {children}
