d251318945bf8a09a72b99266825cb228a972f46
digmi123 <65775879+digmi123@users.noreply.github.com>
2025-08-11 11:49:22 +0300

    Add next.config for github pages.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,18 @@
 import type { NextConfig } from "next";
 import type { Configuration } from "webpack";
 
+const repoName = "Portfolio";
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  output: "export", // Required for static export
+  images: {
+    unoptimized: true, // Disable default image optimization (needed for `next export`)
+  },
+  assetPrefix: isProd ? `/${repoName}/` : "",
+  basePath: isProd ? `/${repoName}` : "",
+
   webpack(config: Configuration) {
     config.module?.rules?.push({
       test: /\.svg$/,
